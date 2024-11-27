deb0df885967753f6dca63a13766195630e73b70
Gabriel <prado1939@live.com>
2024-11-26 23:31:18 -0300

    estudos dia 2

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos = estudar
+        // estudem a classe String que representa texto na aplicação
+
+
+        double  salarioMinimo = 2500;
+
+        short numeroCurto = 1; 
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final  double Valor_De_Pi = 3.14;
+
+
     }
 }
